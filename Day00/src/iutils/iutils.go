4802e45b99e25f1971d8c8048c6ff5fbedd438e4
emmonbea <moskaleviluak@icloud.com>
2024-11-20 18:51:07 +0500

    feat: add iutils package

--- Day00/src/iutils/iutils.go
+++ Day00/src/iutils/iutils.go
@@ -1,3 +1,4 @@
+// Package iutils provides utility functions for reading and validating integer input from standard input.
 package iutils
 
 import (
@@ -7,6 +8,13 @@ import (
 	"strconv"
 )
 
+const (
+	minValue = -100000
+	maxValue = 100000
+)
+
+// ReadInput reads integers from standard input, validates them, and returns a slice of valid integers.
+// It returns an error if there is an issue with reading the input.
 func ReadInput() ([]int, error) {
 	scanner := bufio.NewScanner(os.Stdin)
 	var numbers []int
@@ -30,6 +38,8 @@ func ReadInput() ([]int, error) {
 	return numbers, nil
 }
 
+// validateInput validates that the input string is a valid integer within the range [-100000, 100000].
+// It returns the integer value and an error if the input is invalid.
 func validateInput(input string) (int, error) {
 	num, err := strconv.Atoi(input)
 
@@ -37,7 +47,7 @@ func validateInput(input string) (int, error) {
 		return 0, fmt.Errorf("Error: incorrect entry %s. An integer is expected.", input)
 	}
 
-	if num < -100000 || num > 100000 {
+	if num < minValue || num > maxValue {
 		return 0, fmt.Errorf("Error: number %d is out of valid range.", num)
 	}
 
