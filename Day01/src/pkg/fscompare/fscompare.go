b6b9478752ada9000fe2ad1418bf63ebf7fa7e9f
emmonbea <moskaleviluak@icloud.com>
2024-11-28 10:57:09 +0500

    feat: update fscompare package

---
+++ Day01/src/pkg/fscompare/fscompare.go
@@ -0,0 +1,60 @@
+package fscompare
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+type FSComparer struct{}
+
+func NewFSComparer() *FSComparer {
+	return &FSComparer{}
+}
+
+func (c *FSComparer) CompareDumps(oldFile, newFile string) {
+	oldSet, err := c.readFile(oldFile)
+	if err != nil {
+		log.Fatalf("read file error: %v", err)
+	}
+
+	newSet, err := c.readFile(newFile)
+	if err != nil {
+		log.Fatalf("read file error: %v", err)
+	}
+
+	for path := range oldSet {
+		if _, ok := newSet[path]; !ok {
+			fmt.Printf("ADDED %s\n", path)
+		}
+	}
+
+	for path := range newSet {
+		if _, ok := oldSet[path]; !ok {
+			fmt.Printf("REMOVED %s\n", path)
+		}
+	}
+}
+
+func (c *FSComparer) readFile(filename string) (map[string]struct{}, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	fileSet := make(map[string]struct{})
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		fileSet[line] = struct{}{}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return fileSet, nil
+}
