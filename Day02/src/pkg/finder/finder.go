6341f3daaf1922dd50aa116509721c6195e40d8b
emmonbea <moskaleviluak@icloud.com>
2024-12-03 13:34:05 +0500

    feat: add finder package

---
+++ Day02/src/pkg/finder/finder.go
@@ -0,0 +1,65 @@
+package finder
+
+import (
+	"io/fs"
+	"os"
+	"path/filepath"
+)
+
+type EntryType int
+
+const (
+	File EntryType = iota
+	Directory
+	Symlink
+)
+
+type Entry struct {
+	Path string
+	Type EntryType
+	Link string
+}
+
+type Options struct {
+	IncludeFiles       bool
+	IncludeDirectories bool
+	IncludeSymlinks    bool
+	ExtensionFilter    string
+}
+
+func Find(root string, options Options) ([]Entry, error) {
+	var results []Entry
+
+	err := filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return nil
+		}
+
+		if d.IsDir() && options.IncludeDirectories {
+			results = append(results, Entry{Path: path, Type: Directory})
+		}
+
+		if d.Type().IsRegular() && options.IncludeFiles {
+			if options.ExtensionFilter == "" || filepath.Ext(d.Name()) == "."+options.ExtensionFilter {
+				results = append(results, Entry{Path: path, Type: File})
+			}
+		}
+
+		if d.Type()&os.ModeSymlink != 0 && options.IncludeSymlinks {
+			linkTarget, err := os.Readlink(path)
+			if err != nil {
+				linkTarget = "[broken]"
+			} else {
+				absLinkTarget := filepath.Join(filepath.Dir(path), linkTarget)
+				if _, err := os.Stat(absLinkTarget); err != nil {
+					linkTarget = "[broken]"
+				}
+			}
+			results = append(results, Entry{Path: path, Type: Symlink, Link: linkTarget})
+		}
+		return nil
+
+	})
+
+	return results, err
+}
