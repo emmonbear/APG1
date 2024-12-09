b0310993bb375d161e7a1faa5cc172f0b897fb71
emmonbea <moskaleviluak@icloud.com>
2024-12-09 09:03:21 +0500

    feat(rotate): add a flag handling function

---
+++ Day02/src/pkg/rotate/rotate.go
@@ -0,0 +1,32 @@
+package rotate
+
+import (
+	"flag"
+	"fmt"
+)
+
+type Config struct {
+	FileNames  []string
+	ArchiveDir string
+}
+
+func ParseFlags(args []string) (*Config, error) {
+	fs := flag.NewFlagSet("rotate", flag.ExitOnError)
+
+	var archiveDir string
+	fs.StringVar(&archiveDir, "a", "", "Directory to store archives")
+	if err := fs.Parse(args); err != nil {
+		return nil, err
+	}
+
+	fileNames := fs.Args()
+
+	if len(fileNames) == 0 {
+		return nil, fmt.Errorf("no log files specified")
+	}
+
+	return &Config{
+		ArchiveDir: archiveDir,
+		FileNames:  fileNames,
+	}, nil
+}
